35a158f034b8e0ab609e6d4a9bfe7677277d7bca
Tatyana <ta_ledkova@bk.ru>
2019-01-27 19:35:52 +0400

    Second commit

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+a=5;
+console.log(a) 
+
+
+var number=5;
+var string="Hello!";
+var sym=Symbol();
+var boolean=true;
+null;
+undefined;
+var obj = {};
+
+let persone={
+    name:"John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr=["01.jpg","02.jpg","03.jpg"];
+
+console.log(arr[2])
+
+// alert("Hello world");
+
+// let answer=confirm("Are you here");
+// console.log(answer);
+
+let answer=prompt("Are you here?", "Yes");
+console.log(typeof(answer));
+
+
+
